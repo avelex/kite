cb23b4f9df8294bdce4a5713486dce0fda57cd46
Aleksei Gubin <a.v.gubin0911@gmail.com>
2024-02-25 13:35:47 +0700

    feat: add new handler

--- internal/controllers/http/v1/router.go
+++ internal/controllers/http/v1/router.go
@@ -26,12 +26,17 @@ func (h *handler) RegisterRoutes(r fiber.Router) {
 	v1 := r.Group("/v1")
 	{
 		v1.Get("/status", h.status)
+		v1.Get("/hello", h.hello)
 		v1.Post("/player", h.showPlayerWards)
 	}
 }
 
 func (h *handler) status(ctx *fiber.Ctx) error {
 	return ctx.SendString("ok")
+}
+
+func (h *handler) hello(ctx *fiber.Ctx) error {
+	return ctx.SendString("Hello")
 }
 
 func (h *handler) showPlayerWards(ctx *fiber.Ctx) error {
